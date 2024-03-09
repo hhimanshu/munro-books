f761b436dd87940717b41f32018870411567cb30
hhimanshu <dev.atwork@yahoo.com>
2024-03-09 14:32:38 +1300

    Update package names and fix code formatting

--- packages/data-services/src/lib/db/schemas/Book.ts
+++ packages/data-services/src/lib/db/schemas/Book.ts
@@ -50,10 +50,8 @@ const BookSchema = new mongoose.Schema({
   best_sellers_rank: [BestSellersRankSchema],
 });
 
-
 // Create a text index on the fields 'title', 'author', and 'description'
-BookSchema.index({ title: 'text', author: 'text', description: 'text' });
-
+BookSchema.index({ title: "text", author: "text", description: "text" });
 
 export type Book = mongoose.InferSchemaType<typeof BookSchema>;
 export const Book = mongoose.model("Book", BookSchema);
